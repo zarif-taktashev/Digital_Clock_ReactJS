70e4d709aff527a6a3e421f7b41d3a2c3c849d5f
zarif.taktashev <zarif.taktashev@kcell.kz>
2018-10-25 15:30:31 +0600

    new clock

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class TickTock extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={date: new Date()};
+	}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	componentDidMount(){
+		this.timerID = setInterval(
+			() => this.tick(),
+			1000
+		);
+	}
+
+	componentWillUnmount(){
+		clearInterval(this.timerID);
+	}
+
+	tick(){
+		this.setState({
+			date:new Date()
+		});
+	}
+
+	render(){
+		return(
+			<div>
+				<h1>{this.state.date.toLocaleTimeString()}</h1>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<TickTock />, document.getElementById('root'));
+
